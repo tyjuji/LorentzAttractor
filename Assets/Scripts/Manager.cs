192e827d9da142b4192d415ba2b22ed27a2068b5
Kristian Thiesson <lkr971@alumni.ku.dk>
2019-12-08 23:11:51 +0100

    Initial commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+    public float a = 10f;
+    [SerializeField]
+    public float b = 28.0f;
+    [SerializeField]
+    public float c = 8.0f;
+    [SerializeField]
+    public float t = 0.02f;
+    [SerializeField]
+    public int count = 100;
+    [SerializeField]
+    GameObject _prefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+            Instantiate(_prefab, position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    private Manager _manager = null;
+    private TrailRenderer _trailRenderer = null;
+    private float a;
+    private float b;
+    private float c;
+    private float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _manager = FindObjectOfType<Manager>();
+        _trailRenderer = GetComponent<TrailRenderer>();
+        SetABC();
+        _trailRenderer.startColor = Random.ColorHSV(0f, 1f, 0f, 1f, 0.5f, 0.9f, 1f, 1f);
+        _trailRenderer.endColor = Random.ColorHSV(0f, 1f, 0f, 1f, 0.5f, 0.9f, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //SetABC();
+        float x = gameObject.transform.position.x;
+        float y = gameObject.transform.position.y;
+        float z = gameObject.transform.position.z;
+        float xt = x + t * a * (y - x);
+        float yt = y + t * (x * (b - z) - y);
+        float zt = z + t * (x * y - c * z);
+
+        Vector3 newPosition = new Vector3(xt, yt, zt);
+
+        gameObject.transform.position = newPosition;
+    }
+
+    void SetABC()
+    {
+        a = _manager.a;
+        b = _manager.b;
+        c = _manager.c;
+        t = _manager.t;
+    }
+}
